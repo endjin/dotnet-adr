be7fbba8e247b636dcd2ec5375f5e58cf8216ec0
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2023-06-22 22:41:40 +0100

    Fallback if dirctory isn't a git repo

--- Solutions/Endjin.Adr.Cli/Configuration/FileSystemConfigurationLocator.cs
+++ Solutions/Endjin.Adr.Cli/Configuration/FileSystemConfigurationLocator.cs
@@ -17,7 +17,16 @@ public class FileSystemConfigurationLocator : IConfigurationLocator
 
         while (!Directory.Exists(Path.Combine(directory, ".git")))
         {
-            directory = Directory.GetParent(directory).FullName;
+            if (Directory.GetParent(directory) is not null)
+            {
+                directory = Directory.GetParent(directory)!.FullName;
+            }
+            else
+            {
+                // We've reached the root of the file system, so we'll just use the current directory.
+                directory = Environment.CurrentDirectory;
+                break;
+            }
         }
 
         string configurationPath = Path.Combine(directory, "adr.config.json");
